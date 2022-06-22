10e73259bd1733e15b6c2b0a06b84b3af6260ebb
ElenaGMedero <107564241+ElenaGMedero@users.noreply.github.com>
2022-06-22 16:35:45 +0200

    Update Hola_mundo_Pedro.java

--- src/modelo/Hola_mundo_Pedro.java
+++ src/modelo/Hola_mundo_Pedro.java
@@ -3,7 +3,7 @@ package modelo;
 public class Hola_mundo_Pedro {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, mi nombre es Pedro y tengo 30 años");
+		System.out.println("Hola, mi nombre es Pedro y mi proyecto ha sido modificado por Super Elena");
 
 	}
 
